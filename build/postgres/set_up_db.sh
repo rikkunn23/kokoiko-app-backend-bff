d2bf9bd966f60ae280a9c0e5ac0d09661934577a
rikkunn23 <nemuke.vs.r1kkunn@gmail.com>
2025-05-22 02:05:56 +0900

    コンテナ立ち上げ時のDBのシェルを作成

--- build/postgres/set_up_db.sh
+++ build/postgres/set_up_db.sh
@@ -9,33 +9,21 @@ su postgres -c "initdb -D /var/lib/postgresql/data"
 echo host all all 0.0.0.0/0 trust>>/var/lib/postgresql/data/pg_hba.conf
 su postgres -c "/usr/lib/postgresql/14/bin/pg_ctl start -D ${PGDATA} -s  -w -t 300"
 su postgres -c "/usr/lib/postgresql/14/bin/pg_ctl status -D ${PGDATA}"
-psql -h localhost -p 5432 -U postgres -d postgres -c "create role free_admin with superuser login password 'password123';"
+psql -h localhost -p 5432 -U postgres -d postgres -c "CREATE ROLE kokoiko_admin with superuser login PASSWORD 'password123';"
+psql -h localhost -p 5432 -U postgres -d postgres -c "CREATE DATABASE kokoiko OWNER kokoiko_admin;"
 # DDLの実行
 dirs=(
     "ddl/init/roles"
     "ddl/init/schemas"
-    "ddl/extension"
-    "ddl/function"
     "ddl/create/master"
-    "ddl/create/parent"
-    "ddl/create/child"
-    "ddl/create/grandchild"
-    "ddl/alter"
-    "ddl/post/grants"
-    "ddl/post/grants/FUNCTION"
-    "ddl/post/grants/SEQUENCE"
-    "ddl/post/grants/TABLE"
     "seed/master"
-    "seed/parent"
-    "seed/child"
-    "seed/grandchild"
 )
 for dir in ${dirs[@]}; do
     echo $dir"を実行";
     cd /postgres/$dir
     for file in `\find . -type f -name "*.sql"`; do
         echo $dir$file"を実行";
-        psql -h localhost -p 5432 -U free_admin -d postgres -f $file
+        psql -h localhost -p 5432 -U kokoiko_admin -d kokoiko -f "$file"
     done
 done
 echo "DATABASE IS READY!!";
